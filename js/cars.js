7dc10a237606cfe44f20d15a51af33fcdd72fc77
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-04-25 07:36:32 +0200

    continuing the player select development

--- js/cars.js
+++ js/cars.js
@@ -4,11 +4,12 @@ class Car {
     //Todo lo que se contiene dentro de estas llaves corresponde al plano del coche
 
     //Las propiedades van a entrar a través del constructor
-    constructor(color, estampado, vel_max ) {
+    constructor(color, estampado, transmisión, velocidad, resistencia) {
         this.color = color;
         this.estampado = estampado;
-        this.vel_max = vel_max;
-        this.ruedas = 4;
+        this.transmisión = transmisión;
+        this.velocidad = velocidad;
+        this.resistencia = resistencia;
         this.hp = 100;
 
     }
@@ -28,7 +29,7 @@ class Car {
 
 };
 
-let car1 = new Car("Gris","Liso",100);
-let car2 = new Car("Morado","Llamas",120);
-let car3 = new Car("Rojo","88",100);
-let car4 = new Car("Verde","Llamas",110);
+let car1 = new Car("Gris","Liso",5,35,40);
+let car2 = new Car("Morado","Llamas",7,45,30);
+let car3 = new Car("Rojo","88",6,40,35);
+let car4 = new Car("Verde","Llamas",8,50,25);
