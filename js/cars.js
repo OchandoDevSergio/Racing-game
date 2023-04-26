1836f5354d3ae9cad6a3947361c50cd7de8291b2
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-04-27 01:00:49 +0200

    building the game page view

--- js/cars.js
+++ js/cars.js
@@ -4,7 +4,8 @@ class Car {
     //Todo lo que se contiene dentro de estas llaves corresponde al plano del coche
 
     //Las propiedades van a entrar a través del constructor
-    constructor(color, estampado, transmisión, velocidad, resistencia) {
+    constructor(name, color, estampado, transmisión, velocidad, resistencia) {
+        this.name =name;
         this.color = color;
         this.estampado = estampado;
         this.transmisión = transmisión;
@@ -29,7 +30,7 @@ class Car {
 
 };
 
-let car1 = new Car("Gris","Liso",5,35,40);
-let car2 = new Car("Morado","Llamas",7,45,30);
-let car3 = new Car("Rojo","88",6,40,35);
-let car4 = new Car("Verde","Llamas",8,50,25);
+let car1 = new Car("car1", "Gris","Liso",5,35,40);
+let car2 = new Car("car2", "Morado","Llamas",7,45,30);
+let car3 = new Car("car3", "Rojo","88",6,40,35);
+let car4 = new Car("car4", "Verde","Llamas",8,50,25);
--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 
+//Variables de juego
+
+let ganador = "";
+let player1;
+let player2;
+let nameCarPlayer1;
+let nameCarPlayer2;
+
 const Pageselector = (numpage) => {//función para mostrar una página concreta y ocultar el resto estando todas en el mismo .html
     let arrPages = ["page1", "page2", "page3", "page4"];
     let goingPage = "page" + numpage;
@@ -11,6 +19,47 @@ const Pageselector = (numpage) => {//función para mostrar una página concreta 
 
 }
 
-const Carshowing = (car) => {
-  console.log(car);
+const selectCar = (car) => {
+  
+  //Dado que si player1 es undefined, se puede representar con una exclamación
+  //delante. Eso equivale a que no hemos escogido aun ningun elemento, por lo
+  //tanto, el primero va al player1
+  if(!player1){
+    player1 = car;
+    nameCarPlayer1 = car.name;
+    return;
+  }
+
+  player2 = car;
+  nameCarPlayer2 = car.name;
+  document.getElementById("car1").onclick = null;
+  document.getElementById("car2").onclick = null;
+  document.getElementById("car3").onclick = null;
+  document.getElementById("car4").onclick = null;
+
+  //Si llegamos a este punto en el código, hemos escogido ya dos coches..uno
+  //para el player1 y otro para el player2 ...
+
+  
+  setTimeout(()=>{
+
+    console.log(player1, player2);
+    Pageselector(3)
+
+    buildRacingView();
+  },500);
+
+
+}
+
+
+const buildRacingView = () => {
+
+  let page3 = document.getElementById("page3");
+
+  page3.innerHTML = `<div class="centrar"><div id="page3row1"><div class="carpic" id="carpicplayer1"></div>
+  <div id="page3spacebetween"></div><div class="carpic" id="carpicplayer2"></div>
+</div></div>`;
+document.getElementById("carpicplayer1").classList.add(nameCarPlayer1);
+document.getElementById("carpicplayer2").classList.add(nameCarPlayer2);
 }
